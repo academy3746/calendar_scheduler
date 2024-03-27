b160c2c853091e0750bc63dc3971bcb517687eca
code_otaku <academy3746@gmail.com>
2024-03-27 23:06:45 +0900

    :zap: dotEnv 패키지를 활용한 보안성 강화 패치

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
 
   await initializeDateFormatting();
 
